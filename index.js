454989a3b5be4f6770a13c346b92d182f7c0891f
shrabon-sarker <shrabon.developer@gamil.com>
2021-04-01 10:56:39 +0600

    this is aready day 5

--- index.js
+++ index.js
@@ -1,119 +1,116 @@
-console.log('this is a best out pur')
+// let country = "Bangladesh";
+// console.log(country)
+// console.log(country[4])
+// console.log(country.charAt(4))
+// console.log(country.length)
 
-/*his si a a gootd*/
-console.log("t")
+// let mag = "please Repeat are the 'repeat' word";
+// console.log(mag.charAt(5))
+// console.log(mag.charCodeAt(5))
+// console.log(mag.toUpperCase())
+// console.log(mag.toLowerCase())
+// console.log(mag.includes('repeat', 13))
+// console.log(mag.startsWith('are', 14))
+// console.log(mag.endsWith('Repeat ', 14))
 
-// this is  variables rolls 
-let currentUserAge = "somethik";
-let CurrentUserAge = "this is";
-let current_user_age = "this is "
+// search(), ondexOf(),lastIndexOf()
 
-let price =  125;
-price = 100;
-const name = "john";
-//name="biksh"
-console.log(price, name)
+// let mag = "please are repeat the 'repeat' word"; 
+// console.log(mag.search('are')) // full
+// console.log(mag.indexOf('repeat', 14)) //start  repeat' word
+// console.log(mag.lastIndexOf('are', 14)) // last please are repeat the 
 
-// data type
-let  fullname ="biksah";
+// console.log(mag.search('the'));
+// console.log(mag.indexOf('repeaty', 16))
+// console.log(mag.lastIndexOf('word', 16))
 
-console.log( typeof name)
-let age = 25;
-console.log(typeof age);
+// ==== ==== toString(), Concat(), split() ======
 
-let isMarried = true;
-console.log( typeof isMarried)
+//toString()
+// let tem = 155;
+// console.log(typeof tem.toString())
+// let m1 = "this "
+// let m2 = "is ";
+// let m3 = "your ";
+// let m4 = "name"
 
-let colors = undefined;
-console.log(typeof colors)
-
-let selectedColors =  null;
-console.log(typeof selectedColors)
-
-
- // Object
-
- let user = {
-     name: "john",
-     age: 41,
-     hobby: 'reading'
- }
-
- console.log(user)
- console.log(user.name)
- console.log(user.age)
- console.log(user.hobby)
-
- console.log(user['name']);
- console.log(user['hobby'])
- console.log(user['age'])
+// // concat()
+// let total = m1.concat(m1, m3, m4)
+// console.log(total)
+// tolat = m1 + m2+ m3+ m4;
+// console.log(total)
 
 
- //Array 
-
- let friends = ['john', 'mary', 'smith', 'target'];
- console.log(friends)
-
- console.log(friends[1])
- console.log(friends[2])
-
- console.log(friends.length)
- console.log(friends[3].length)
-
-// function 
-
-function showMyName(name){
-    console.log(name)
-    console.log('my name is ' + name)
-}
-
-showMyName('john');
-showMyName('mary');
+// // split()
+// let mag = "please are repeat the 'repeat' word"; 
+// console.log(mag.split("e", 2)) // " ", '', e (last parmiter limit kaj kre)
 
 
-function cal(num1, num2) {
-    const sum = num1 + num2
-    //console.log(sum)
-    return sum;
-}
+// ============= slice(), subStr(), subString() =========
 
- const result = cal(4,7);
- console.log(result)
+// slice()
+let mag = "please are repeat the 'repeat' word."; 
+console.log(mag.slice(0, 6)) // index akare value ney
+console.log(mag.slice(5, 20))
+console.log(mag.slice(5))
+console.log(mag.slice())
+console.log(mag.slice(0))
+console.log(mag.slice(-10, -1))
 
- function marketing(bus, bike) {
-     const taka = bus + bike;
-     return taka;
- }
+// subString() 
 
- const stup = marketing(200, 36);
- console.log(stup)
+console.log(mag.substring(0,6)) // index are value ney
+// substr()
+console.log(mag.substr(0, 14)) // length akare value ney
 
- // template literals
+let message = '\tHe said, "It\'s a \tsunny day.\nHave  a good day.'
+console.log(message)
 
- let myName = 'smith';
- console.log(`this is a name : ${myName}`)
 
- let message = `hi 
-how are you?`;
- console.log(message)
+let mags = "rikass";
+mags = "bikash" + " shrabon"
+console.log(mags)
 
- const Person = {
-     name: 'bikash',
-     age : 24,
-     isMarried: true,
-     homeAddress: {
-         long: 48.36,
-         lat: 52.32
-     },
-     friends: ['john','mary', 'smith']
- }
-console.log(Person)
-console.log(Person.name)
-console.log(Person.age)
-console.log(Person.homeAddress.lat)
-console.log(Person.homeAddress.long)
-console.log(Person.friends[2])
-console.log(Person.friends.length)
-console.log(Person.friends[1].length)
+// replace()
 
- 
+let str = "this is kame : bikash";
+let res = str.replace('kame', 'name')
+console.log(str)
+console.log(res)
+
+// trim()
+
+let name = "      shakip sraker     ";
+console.log(name);
+
+let rem = name.trim();
+console.log(rem)
+
+// mathch()
+
+let pro = " this si a name bikash are. shra , bikash , the name bikash";
+pro = pro.match(/bikash/g);
+console.log(pro)
+
+
+
+
+// repeat()
+let name1 = "bikash ";
+name1.repeat(6)
+console.log(name1.repeat(20))
+
+let alni = ' cat cat cat cat';
+console.log(alni.replaceAll(/cat/g, 'dog'))
+
+let n = "2545";
+console.log(parseInt(n))
+
+let mac = " this is a dom";
+console.log(mac.match(/dom/g))
+
+console.log(mac.substr(11, 3))
+
+
+let muStr = `Prograssing`;
+console.log(muStr.replaceAll("s", 'm'))
